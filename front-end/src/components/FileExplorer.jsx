3808dceb3bb81ecce98c19e4fa570cacd3422e62
Akshat <mangalakshat01@gmail.com>
2025-01-19 11:02:12 +0530

    File explorer should be able to handle large file names

--- front-end/src/components/FileExplorer.jsx
+++ front-end/src/components/FileExplorer.jsx
@@ -1,36 +1,52 @@
 import React, { useState, useEffect, useContext } from "react";
 import { SimpleTreeView, TreeItem } from "@mui/x-tree-view";
-import {
-  Folder,
-  InsertDriveFile,
-} from "@mui/icons-material";
+import { Folder, InsertDriveFile } from "@mui/icons-material";
 import { DashboardContext } from "../context/DashboardContext";
+import Tooltip from "@mui/material/Tooltip";
 
 const FileExplorer = () => {
-  const { selectedNodeId, handleSelectNodeId, allFileData} = useContext(DashboardContext);
+  const { selectedNodeId, handleSelectNodeId, allFileData } =
+    useContext(DashboardContext);
 
   const handleDirectorySelect = (event, nodeId) => {
     handleSelectNodeId(nodeId);
-  }
-  
-  const renderTree = (nodes) => {
-    if (!nodes.id) {
-      nodes.id = `generated-id-${nodes.name}`;
-    }
+  };
 
-    return (
-      <TreeItem
-        key={nodes.id}
-        itemId={nodes.id}
-        label={nodes.name}
-        icon={nodes.isFile ? <InsertDriveFile /> : <Folder />}
-        sx={{ textAlign: 'left' }}
-      >
-        {Array.isArray(nodes.children)
-          ? nodes.children.map((child) => renderTree(child))
-          : null}
-      </TreeItem>
-    );
+  const truncateFileName = (name, maxLength = 20) => {
+    if (name.length > maxLength) {
+      return `${name.slice(0, maxLength)}...`;
+    }
+    return name;
+  };
+
+  const maxLength = 15;
+  
+
+  const renderTree = (nodes) => {
+    if (nodes.id) {
+      const isTruncated = nodes.name.length > maxLength;
+      const truncatedName = truncateFileName(nodes.name, maxLength);
+      const label = isTruncated ? (
+        <Tooltip title={nodes.name} placement="top">
+          <span>{truncatedName}</span>
+        </Tooltip>
+      ) : (
+        <span>{nodes.name}</span>
+      );
+      return (
+        <TreeItem
+          key={nodes.id}
+          itemId={nodes.id}
+          label={label}
+          icon={nodes.isFile ? <InsertDriveFile /> : <Folder />}
+          sx={{ textAlign: "left" }}
+        >
+          {Array.isArray(nodes.children)
+            ? nodes.children.map((child) => renderTree(child))
+            : null}
+        </TreeItem>
+      );
+    }
   };
 
   return (
