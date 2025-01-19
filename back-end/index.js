055106eed58565e2428e666a253bc90e740e272b
Akshat <mangalakshat01@gmail.com>
2025-01-19 14:45:29 +0530

    Update for cors error

--- back-end/index.js
+++ back-end/index.js
@@ -1,39 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const {errorHandler} = require('./controller');
-const bodyParser = require('body-parser');
-const router = require('./routes/index.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { errorHandler } = require("./controller");
+const bodyParser = require("body-parser");
+const router = require("./routes/index.js");
 dotenv.config();
 const app = express();
 
-
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.json());
-app.use('/', router);
+app.use("/", router);
 app.use(errorHandler);
 
 const DB_URL = process.env.DB_SERVER;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-        console.log("DB connected");
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  
 // start the server
 const start = async () => {
-    try {
-        await connectDB();
-        app.listen(process.env.PORT);
-    } catch (error) {
-        console.log(error);
-    }
-}
-  
+  try {
+    await connectDB();
+    app.listen(process.env.PORT);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 start();
